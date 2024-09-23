5c8133a26e69c9786c2464c249686fe51698e30a
Eunji0301 <forstudy.eunji@gmail.com>
2024-09-23 12:27:30 +0900

    servletTest

---
+++ webPr/src/main/java/test/ServeletTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ServeletTest")
+public class ServeletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		System.out.println("서블릿 테스트입니다.");
+		
+		PrintWriter out = response.getWriter();
+		out.println("hello world");
+		out.println("한글 테스트");
+		
+		out.println("<!doctype=html>"
+				+ "<html>"
+				+ "<head>"
+				+ "<title>제목란</title>"
+				+ "</head>"
+				+ "<body><span style=\"color:red;font-size:20px;\">내용</span></body>"
+				+ "</html>");
+	
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
